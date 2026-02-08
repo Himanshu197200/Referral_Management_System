920a33baf0982c7d671f14c7d8588fba576c9084
Himanshu Mishra <himanshumishra4926@gmail.com>
2026-02-08 23:21:02 +0530

    Add auth styles and token interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -11,6 +11,14 @@ const api = axios.create({
     headers: { 'Content-Type': 'application/json' }
 });
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = 'Bearer ' + token;
+    }
+    return config;
+});
+
 export const candidateService = {
     getAllCandidates: async (filters = {}) => {
         try {
